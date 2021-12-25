06e06299bf8c977ed9bfec5d167980e69b08e04b
Espen Riskedal <espen@agelabs.com>
2021-12-25 20:26:34 +0100

    feat: exclude stablecoins

--- index.R
+++ index.R
@@ -1,7 +1,13 @@
+library(dplyr)
 library(httr)
 library(jsonlite)
 
+marketcap_response <- GET("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc")
+marketcap <- fromJSON(content(marketcap_response, as = "text"), flatten = TRUE)
 
-response <- GET("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc")
+stablecoins_response <- GET("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=stablecoins&order=market_cap_desc")
+stablecoins <- fromJSON(content(stablecoins_response, as = "text"), flatten = TRUE)
 
-market_cap <- fromJSON(content(response, as = "text"), flatten = TRUE)
+marketcap_excluding_stablecoins <- marketcap %>%
+  filter(!symbol %in% stablecoins$symbol) %>%
+  arrange(market_cap_rank)
