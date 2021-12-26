a7565cd9326a71347ba6cfc15e3908f5089db471
Espen Riskedal <espen@agelabs.com>
2021-12-26 11:16:29 +0100

    feat: piechart top 10
    - include only top 100 binance symbols

--- index.R
+++ index.R
@@ -1,3 +1,4 @@
+library(ggplot2)
 library(dplyr)
 library(httr)
 library(jsonlite)
@@ -8,6 +9,23 @@ marketcap <- fromJSON(content(marketcap_response, as = "text"), flatten = TRUE)
 stablecoins_response <- GET("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&category=stablecoins&order=market_cap_desc")
 stablecoins <- fromJSON(content(stablecoins_response, as = "text"), flatten = TRUE)
 
+binance_response <- GET("https://api.coingecko.com/api/v3/exchanges/binance")
+binance <- fromJSON(content(binance_response, as = "text"), flatten = TRUE)
+binance_symbols <- tolower(unique(c(binance$tickers$base, binance$tickers$target)))
+
 marketcap_excluding_stablecoins <- marketcap %>%
+  filter(symbol %in% binance_symbols) %>% 
   filter(!symbol %in% stablecoins$symbol) %>%
-  arrange(market_cap_rank)
+  arrange(market_cap_rank) %>%
+  slice_head(n = 10)
+
+# Create Data
+data <- data.frame(
+  group=marketcap_excluding_stablecoins$symbol,
+  value=marketcap_excluding_stablecoins$market_cap
+)
+
+# Basic piechart
+ggplot(data, aes(x="", y=value, fill=group)) +
+  geom_bar(stat="identity", width=1) +
+  coord_polar("y", start=0)
