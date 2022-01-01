a5b6058f21e694cd6280ea0e7d3fe55e763e2a45
Espen Riskedal <espen@agelabs.com>
2022-01-01 20:49:33 +0100

    feat: use sqrt of market cap

--- index.R
+++ index.R
@@ -16,16 +16,19 @@ binance_symbols <- tolower(unique(c(binance$tickers$base, binance$tickers$target
 marketcap_excluding_stablecoins <- marketcap %>%
   filter(symbol %in% binance_symbols) %>% 
   filter(!symbol %in% stablecoins$symbol) %>%
+  mutate(market_cap_squared = sqrt(market_cap)) %>%
   arrange(market_cap_rank) %>%
   slice_head(n = 10)
 
 # Create Data
 data <- data.frame(
-  group=marketcap_excluding_stablecoins$symbol,
-  value=marketcap_excluding_stablecoins$market_cap
+  group = marketcap_excluding_stablecoins$symbol,
+  value = marketcap_excluding_stablecoins$market_cap_squared,
+  percentage = marketcap_excluding_stablecoins$market_cap_squared /
+    sum(marketcap_excluding_stablecoins$market_cap_squared)
 )
 
 # Basic piechart
-ggplot(data, aes(x="", y=value, fill=group)) +
+ggplot(data, aes(x="", y=value, fill=group)) + 
   geom_bar(stat="identity", width=1) +
   coord_polar("y", start=0)
